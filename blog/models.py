3215d8774b6d587bf1b562d03ab7544b6e6d7484
tehilad <tehiladaboush@gmail.com>
2019-06-02 14:22:58 +0300

    Changed the HTML for the site.

--- blog/models.py
+++ blog/models.py
@@ -14,16 +14,5 @@ class Post(models.Model):
         self.published_date = timezone.now()
         self.save()
 
-class stam(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    title = models.CharField(max_length=200)
-    text = models.TextField()
-    created_date = models.DateTimeField(default=timezone.now)
-    published_date = models.DateTimeField(blank=True, null=True)
-
-    def publish(self):
-        self.published_date = timezone.now()
-        self.save()
-
     def __str__(self):
         return self.title
